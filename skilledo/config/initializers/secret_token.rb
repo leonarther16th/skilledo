b676fc47817c1929bf325265fe811268b915e26c
Moe Alkhatib <leonarther16th@gmail.com>
2014-06-08 23:07:51 -0400

    First Commit

---
+++ skilledo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skilledo::Application.config.secret_key_base = 'fb0b95a149f1ad562b4d131625821cbd104dc7c8e5a929de872227086bb8ac021aba46580407a211460e8e4849100656153d8a28a5b4bc8f222211fe1ebf9335'
---
+++ skilledo/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skilledo::Application.config.session_store :cookie_store, key: '_skilledo_session'
